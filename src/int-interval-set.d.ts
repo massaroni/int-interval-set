0e8cadac85ccd247d400e5b4a32a6607f53c1ac7
Kevin Massaroni <kevinmassaroni@gmail.com>
2021-05-16 00:48:45 -0400

    typescript definitions

---
+++ src/int-interval-set.d.ts
@@ -0,0 +1,22 @@
+declare module 'int-interval-set' {
+    interface Interval {
+        lower: number
+        upper: number
+    }
+
+    class IntIntervalSet {
+        public clone (): IntIntervalSet
+        public union (lower: number, upper?: number): IntIntervalSet
+        public contains (point: number): boolean
+        public *values (): Generator<number, void, void>
+        public unionAll (intervals: Interval[])
+        public complement (): IntIntervalSet
+        public intersection (lower: number, upper: number): IntIntervalSet
+        public isFull (): boolean
+        public isEmpty (): boolean
+        public span (): Interval
+        public spanSet (): IntIntervalSet
+    }
+
+    export = IntIntervalSet
+}
